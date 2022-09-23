dc2864275236aabd847a5fc57dc528830f875fa2
sardaryandavid <74458820+sardaryandavid@users.noreply.github.com>
2022-09-23 19:04:48 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+enum {NO = 0, YES};
+
+#define EPS 10e-6
+struct Complex
+{
+    double r;
+    double im;
+
+    Complex ();
+    Complex (const Complex& );
+    Complex (double, double);
+    ~Complex ();
+
+    friend Complex operator+ (const Complex&, const Complex& );
+    friend Complex operator- (const Complex&, const Complex& );
+
+    friend Complex operator+ (const Complex&, const double );
+    friend Complex operator- (const Complex&, const double );
+    Complex operator+ (const Complex& ) const;
+    Complex operator- (const Complex& ) const;
+    Complex operator* (const Complex& ) const;
+    Complex operator/ (const Complex& ) const;
+    Complex operator= (const Complex& );
+
+    int operator>  (const Complex& ) const;
+    int operator>= (const Complex& ) const;
+    int operator<  (const Complex& ) const;
+    int operator<= (const Complex& ) const;
+    int operator== (const Complex& ) const;
+    int operator!= (const Complex& ) const;
+    
+    void print () const;
+    double cmplxAbs () const;
+};
+
+/*************************************************************/
+
+Complex::Complex ()
+{
+    r = 0;
+    im = 0;
+};
+
+/*************************************************************/
+
+Complex::Complex (double realPart, double imPart) 
+{
+    r = realPart;
+    im = imPart;
+}
+
+/*************************************************************/
+
+Complex::Complex (const Complex& complexNum)
+{
+    r = complexNum.r;
+    im = complexNum.im;
+}
+
+/*************************************************************/
+
+Complex Complex::operator= (const Complex& ComplexNum)
+{
+    return Complex (ComplexNum.r, ComplexNum.im);
+}
+
+/*************************************************************/
+
+double Complex:: cmplxAbs () const
+{
+    return this->r * this->r + this->im * this->im;
+}
+
+/*************************************************************/
+
+Complex Complex::operator+(const Complex& cmplxNum) const
+{
+    return Complex (this->r + cmplxNum.r, this->im + cmplxNum.im);
+}
+
+/*************************************************************/
+
+Complex operator+(const Complex& cmplxNum, const double num)
+{ 
+    return Complex (cmplxNum.r + num, cmplxNum.im); 
+}
+
+/*************************************************************/
+
+Complex operator-(const Complex& cmplxNum, const double num)
+{
+    return Complex (cmplxNum.r - num, cmplxNum.im);
+}
+
+/*************************************************************/
+
+Complex Complex::operator-(const Complex& cmplxNum) const
+{
+    return Complex (this->r - cmplxNum.r, this->im - cmplxNum.im);
+}
+
+/*************************************************************/
+
+Complex Complex::operator*(const Complex& cmplxNum) const
+{
+    return Complex (this->r * this->im - cmplxNum.r * cmplxNum.im, 
+                    this->r * cmplxNum.im - this->im * cmplxNum.r);
+}
+
+/*************************************************************/
+
+Complex Complex::operator/(const Complex& cmplxNum) const
+{
+    int val = cmplxNum.im * cmplxNum.im + cmplxNum.r * cmplxNum.r;
+
+    return Complex ((this->r * cmplxNum.r + this->im * cmplxNum.im) / val,
+                    (this->im * cmplxNum.r - this->r * cmplxNum.im) / val);
+}
+/*************************************************************/
+
+Complex::~Complex ()
+{
+    printf ("I was in destructor\n");
+}
+
+/*************************************************************/
+
+void Complex::print () const
+{
+    printf ("%.02f + (%.02f)i\n", this->r, this->im);
+}
+
+/*************************************************************/
+
+int Complex::operator> (const Complex& cmplxNum) const 
+{
+    return (this->cmplxAbs () - cmplxNum.cmplxAbs () > EPS) ? YES : NO;
+}
+
+int Complex::operator>= (const Complex& cmplxNum) const 
+{
+    int diff = this->cmplxAbs() - cmplxNum.cmplxAbs();
+    return (diff > 0 || fabs(diff) < EPS) ? YES : NO;
+}
+
+int Complex::operator< (const Complex& cmplxNum) const 
+{
+    return !(*this >= cmplxNum);
+}
+
+int Complex::operator<= (const Complex& cmplxNum) const 
+{
+    return !(*this > cmplxNum);
+}
+
+int Complex::operator== (const Complex& cmplxNum) const 
+{
+    return (fabs(this->r - cmplxNum.r) < EPS && fabs(this->im - cmplxNum.im) < EPS); 
+}
+
+int Complex::operator!= (const Complex& cmplxNum) const 
+{
+    return !(*this == cmplxNum); 
+}
+
+/*************************************************************/
+
+Complex operator+ (const Complex& cmplx1, const Complex& cmplx2)
+{
+    return Complex (cmplx1.r + cmplx2.r, cmplx1.im + cmplx2.im);
+}
+
+/*************************************************************/
+
+Complex operator- (const Complex& cmplx1, const Complex& cmplx2)
+{
+    return Complex (cmplx1.r - cmplx2.r, cmplx1.im - cmplx2.im);
+}
+
+/*************************************************************/
+
+int main()
+{
+    Complex compl1 {1.0, 2.0};
+    Complex compl2 {2, 1};
+    Complex compl3 {100, 100};
+    printf ("%d\n", compl3 > compl1);
+
+    compl3.print();
+
+    return 0;
+}
